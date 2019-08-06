e50825b6b37c22945610c00c594b616dd86ddc5f
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-06 20:06:47 -0300

    começa a api

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://root:root@cluster0-jd2p9.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+server.use(express.json());
+server.use(routes);
+server.listen(3031);
