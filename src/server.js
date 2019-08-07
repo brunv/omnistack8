1b691085543155d64088ae612ec67938888d12e8
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-06 22:10:16 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
 
 mongoose.connect('mongodb+srv://root:root@cluster0-jd2p9.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 server.listen(3031);
